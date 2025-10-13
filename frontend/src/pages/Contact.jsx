38b8943a7260ced43f464cedfcb488918c8aa74b
venkat sai <saik87630@gmail.com>
2025-10-13 16:32:43 +0530

    chnmages

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import Title from '../components/Title'
-import { assets } from '../assets/assets'
-import NewsLetterBox from '../components/NewsLetterBox'
+import React from 'react';
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/NewsLetterBox';
 
 const Contact = () => {
   return (
     <div>
+      {/* Header */}
       <div className='pt-10 text-2xl text-center border-t'>
         <Title text1={'CONTACT'} text2={'US'} />
       </div>
+
+      {/* Main Contact Section */}
       <div className='flex flex-col justify-center gap-10 my-10 md:flex-row mb-28'>
-        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="Contact Photo" />
+        {/* Image */}
+        <img
+          className='w-full md:max-w-[480px]'
+          src={assets.contact_img}
+          alt="Contact Photo"
+        />
+
+        {/* Info */}
         <div className='flex flex-col items-start justify-center gap-6'>
           <p className='text-xl font-semibold text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>Trendify 354 Fashion Lane <br />Los Angeles, SC 45678, USA</p>
-          <p className='text-gray-500'>Tel: (+11)-558-669-447 <br />Email: contact.trendify@info.com</p>
-          <p className='text-xl font-semibold text-gray-600'>Careers at Forever</p>
-          <p className='text-gray-500'>Join us at Trendify! Explore job openings and help shape the future of fashion. <br />Explore our current job openings and discover how you can contribute to our mission of setting trends and creating style.</p>
-          <button className='px-8 py-4 text-sm transition-all duration-500 border border-black hover:bg-gray-800 hover:text-white'>Explore Jobs</button>
+          <p className='text-gray-500'>
+            Sahara Store <br />
+            Hyderabad, India
+          </p>
+          <p className='text-gray-500'>
+            Tel: +91-9347175125 <br />
+            Email: sahara.store.online@gmail.com
+          </p>
+
+          <p className='text-xl font-semibold text-gray-600'>Careers at Sahara</p>
+          <p className='text-gray-500'>
+            Join Sahara! Explore job opportunities and help us bring the best Diwali collections to our customers.
+          </p>
+          Coming soon...
         </div>
       </div>
+
+      {/* Newsletter Box */}
       <NewsLetterBox />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
